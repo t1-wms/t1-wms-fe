3bbcbab245d2e1e12664472ec05cd4a23221216b
ektto1041 <dhkdwk1041@gmail.com>
2025-02-16 16:12:08 +0900

    [T1-404] feat: 출고패킹 페이지 추가, 라우팅

---
+++ packages/wms/src/pages/outbound-packing/ui/OutboundPackingPage.tsx
@@ -0,0 +1,69 @@
+import { PageContentBox, Spinner } from "@/shared";
+import styles from "./OutboundPackingPage.module.css";
+import {
+  OutboundPackingDrawer,
+  OutboundPackingResponseDto,
+  OutboundPackingTableWrapper,
+  OutboundControlPanel,
+  OutboundPickingListDrawer,
+} from "@/features";
+import { Suspense, useCallback, useState } from "react";
+import { ColumnFiltersState } from "@tanstack/react-table";
+
+export const OutboundPackingPage = () => {
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+  const [isDrawerOpen, setDrawerOpen] = useState(false);
+  const [selectedRow, setSelectedRow] =
+    useState<OutboundPackingResponseDto | null>(null);
+
+  const handleSearch = useCallback(
+    (number: string, startDate: string, endDate: string) => {
+      setColumnFilters([
+        { id: "outboundPackingDate", value: `${startDate},${endDate}` },
+        { id: "outboundPackingNumber", value: number },
+      ]);
+    },
+    [setColumnFilters]
+  );
+
+  const handleClickCreate = useCallback(() => {
+    setDrawerOpen(true);
+  }, [setDrawerOpen]);
+
+  const handleChangeSelectedRow = useCallback(
+    (row: OutboundPackingResponseDto | null) => {
+      setSelectedRow(row);
+    },
+    [setSelectedRow]
+  );
+
+  return (
+    <div className={styles.container}>
+      <PageContentBox>
+        <OutboundControlPanel
+          label="출고패킹"
+          onSearch={handleSearch}
+          onClickCreate={handleClickCreate}
+        />
+      </PageContentBox>
+      <PageContentBox>
+        <Suspense fallback={<Spinner message="출고패킹 품목을 세는 중" />}>
+          <OutboundPackingTableWrapper
+            columnFilters={columnFilters}
+            setColumnFilters={setColumnFilters}
+            onChangeSelectedRow={handleChangeSelectedRow}
+          />
+        </Suspense>
+      </PageContentBox>
+      {isDrawerOpen && (
+        <OutboundPickingListDrawer onClose={() => setDrawerOpen(false)} />
+      )}
+      {selectedRow && (
+        <OutboundPackingDrawer
+          data={selectedRow}
+          onClose={() => setSelectedRow(null)}
+        />
+      )}
+    </div>
+  );
+};
