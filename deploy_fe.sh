f6c94e5a8eb42ace0921f3e62430602893580b35
joo9837 <joo9837@naver.com>
2025-02-17 16:20:05 +0900

    [T1-445] feat: build 테스트

--- deploy_fe.sh
+++ deploy_fe.sh
@@ -2,16 +2,14 @@
 
 # EC2 서버에서 배포할 경로
 WMS_DIST_PATH="/home/ec2-user/frontend/wms/dist"
-WORKER_DIST_PATH="/home/ec2-user/frontend/worker/dist"
 SHARED_CSS_PATH="/home/ec2-user/frontend/shared/css"
 NGINX_CONFIG_PATH="/home/ec2-user/frontend/config/nginx"
 
 # 필요한 디렉토리 생성
-ssh ec2-user@${EC2_DOMAIN} "mkdir -p ${WMS_DIST_PATH} ${WORKER_DIST_PATH} ${SHARED_CSS_PATH} ${NGINX_CONFIG_PATH}"
+ssh ec2-user@${EC2_DOMAIN} "mkdir -p ${WMS_DIST_PATH} ${SHARED_CSS_PATH} ${NGINX_CONFIG_PATH}"
 
 # 빌드된 파일과 설정 파일을 EC2로 전송
 scp -r packages/wms/dist/* ec2-user@${EC2_DOMAIN}:${WMS_DIST_PATH}
-scp -r packages/worker/dist/* ec2-user@${EC2_DOMAIN}:${WORKER_DIST_PATH}
 scp -r packages/shared/css/* ec2-user@${EC2_DOMAIN}:${SHARED_CSS_PATH}
 scp -r config/nginx/* ec2-user@${EC2_DOMAIN}:${NGINX_CONFIG_PATH}
 
@@ -26,10 +24,10 @@ cd /home/ec2-user/frontend
 sudo cp config/nginx/frontend.conf /etc/nginx/conf.d/
 sudo nginx -t && sudo systemctl reload nginx
 
-# Docker 컨테이너 재시작
-docker-compose down
-docker-compose up -d
+# Docker 컨테이너 재시작 (WMS만)
+docker-compose down wms
+docker-compose up -d wms
 
-echo 'Deployment completed!'
-docker ps
+echo 'WMS Deployment completed!'
+docker ps | grep wms
 EOF
