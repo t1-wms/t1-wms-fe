182c11c84eeae4ad1775ff97e5327e0a69886cf8
KIM_DEAHO <102588838+DHowor1d@users.noreply.github.com>
2025-02-20 16:01:39 +0900

    feat: add splash

--- packages/worker/dev-dist/sw.js
+++ packages/worker/dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-bf09d6d8'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.g5nrjt66kdg"
+    "revision": "0.rqrscb8ot3"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
