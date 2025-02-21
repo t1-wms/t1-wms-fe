dd999892bf75a7e9610f2f762e52c429ea8c9f9f
ektto1041 <dhkdwk1041@gmail.com>
2025-02-21 20:57:51 +0900

    [T1-565] feat: PieChart 수정

--- packages/wms/src/pages/dashboard/ui/DashboardPage.tsx
+++ packages/wms/src/pages/dashboard/ui/DashboardPage.tsx
@@ -1,42 +1,53 @@
-import { Spinner } from "@/shared";
-import styles from "./DashboardPage.module.css";
 import {
   InboundPieChart,
-  OrderBarChart,
+  OrderPieChart,
+  OutboundPieChart,
   ProductThresholdBarChart,
 } from "@/features";
+import { ErrorBoundary, Spinner } from "@/shared";
 import { Suspense } from "react";
-import { OutboundPieChart } from "@/features/outbound/ui/outbound-pie-chart";
+import styles from "./DashboardPage.module.css";
 
 export default function DashBoardPage() {
   return (
     <div className={styles.container}>
       <div className={styles.line}>
         <div className={`${styles.box} shadow-md`}>
+          <h3 className="font-h3">입고현황</h3>
           <Suspense fallback={<Spinner message="입고현황을 가져오는 중" />}>
             <InboundPieChart />
           </Suspense>
         </div>
         <div className={`${styles.box} shadow-md`}>
+          <h3 className="font-h3">출고현황</h3>
           <Suspense fallback={<Spinner message="출고현황을 가져오는 중" />}>
             <OutboundPieChart />
           </Suspense>
         </div>
-      </div>
-      <div className={styles.line}>
         <div className={`${styles.box} shadow-md`}>
+          <h3 className="font-h3">발주현황</h3>
           <Suspense fallback={<Spinner message="발주현황을 가져오는 중" />}>
-            <OrderBarChart />
+            <OrderPieChart />
           </Suspense>
         </div>
       </div>
       <div className={styles.line}>
+        <div className={`${styles["flex-2"]}`}>
+          <div className={`${styles.box} shadow-md`}>
+            <ErrorBoundary fallback={<>에러야~!</>}>
+              <Suspense fallback={<Spinner message="재고현황을 가져오는 중" />}>
+                <ProductThresholdBarChart />
+              </Suspense>
+            </ErrorBoundary>
+          </div>
+        </div>
         <div className={`${styles.box} shadow-md`}>
           <Suspense fallback={<Spinner message="재고현황을 가져오는 중" />}>
             <ProductThresholdBarChart />
           </Suspense>
         </div>
       </div>
+      <div className={styles.line}></div>
     </div>
   );
 }
