4cbe2e69326ad1cd310933cbcbbccde9c6e47d10
ektto1041 <dhkdwk1041@gmail.com>
2025-02-22 09:56:09 +0900

    fix: 빌드 오류 수정

--- packages/wms/src/features/order/ui/order-pie-chart/OrderPieChart.tsx
+++ packages/wms/src/features/order/ui/order-pie-chart/OrderPieChart.tsx
@@ -11,25 +11,18 @@ export const OrderPieChart = () => {
         id: "발주 요청",
         label: "발주 요청",
         value: data.notApproved,
-        color: "#ff0000",
       },
       {
         id: "발주 승인",
         label: "발주 승인",
         value: data.approved,
-        color: "#00ff00",
       },
     ];
   }, [data]);
 
   return (
     <div style={{ width: "100%", aspectRatio: 1 }}>
-      <PieChart
-        title="입고현황"
-        labels={["입하예정", "입하검사", "입고적치"]}
-        dataLabel="입고현황"
-        data={chartData}
-      />
+      <PieChart data={chartData} />
     </div>
   );
 };
